067906c1e178d25506974a6f1f89a0f5abb58648
ahmad <mahmadk2@hotmail.com>
2020-09-20 01:51:24 +0500

    devise and email sender

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ ruby '2.7.1'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.3'
 # Use postgresql as the database for Active Record
 gem 'pg', '>= 0.18', '< 2.0'
+gem 'devise'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,21 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+
+:address => 'smtp.sendgrid.net',
+
+:port => '587',
+
+:authentication => :plain,
+
+:user_name => ENV['mahmadk2'],
+
+:password => ENV['jxtazaw12'],
+
+:domain => 'heroku.com',
+
+:enable_starttls_auto => true
+
+}
